b01c2d0907b38ffe953bfeb869ca910735aa787c
Aman Mavai <amanmavai@gmail.com>
2023-10-14 22:27:43 +0530

    add sample header component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -21,39 +21,16 @@ export function Layout({children, header, footer}: LayoutProps) {
   );
 }
 
-export type HeaderProps = {
-  navItems?: React.JSX.Element[];
-};
-export function Header({navItems = []}: HeaderProps) {
-  const [isOpen, setIsOpen] = React.useState(false);
+export function Header() {
   return (
-    <header className="bg-gray-800 p-4 h-20">
+    <header className="bg-gray-800 p-4 h-20 flex justify-between">
       {/* <!-- Header content --> */}
-      <nav className="">
-        <div className="container mx-auto flex justify-between">
-          <div className="flex">
-            <span className="text-white font-bold">Logo</span>
-          </div>
-          {/* Desktop Menu */}
-          <div className="hidden md:flex">{navItems}</div>
-          {/* Mobile View */}
-          <div className="md:hidden">
-            {/* Hamburger Menu */}
-            <button onClick={() => setIsOpen(!isOpen)}>
-              <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                {isOpen ? (
-                  // 'X' icon for close
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                ) : (
-                  // Hamburger icon
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                )}
-              </svg>
-            </button>
-          </div>
-        </div>
-        {isOpen && <div className="md:hidden">{navItems}</div>}
-      </nav>
+      <div>Logo</div>
+      <div className="flex items-center gap-2">
+        <div>Home</div>
+        <div>About</div>
+        <div>Contact</div>
+      </div>
     </header>
   );
 }
