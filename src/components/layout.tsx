2a972652c9c330617e2dfb3f28a3f2d8b691e922
Aman Mavai <amanmavai@gmail.com>
2023-10-19 16:40:52 +0530

    update layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,11 +28,24 @@ type HeaderProps = {
 
 export function Header({title, links}: HeaderProps) {
   return (
-    <header className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">{title}</div>
-        <nav>
-          <ul className="flex space-x-4">
+    <header className="tw-navbar bg-base-100 border-b border-b-gray-200">
+      <div className="tw-navbar-start">
+        <div className="tw-dropdown">
+          <label tabIndex={0} className="tw-btn tw-btn-ghost tw-btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="tw-menu tw-menu-sm tw-dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
             {links.map((link, index) => (
               <li key={index}>
                 <a href={link.href} className="text-white hover:text-gray-400 transition duration-300">
@@ -41,7 +54,47 @@ export function Header({title, links}: HeaderProps) {
               </li>
             ))}
           </ul>
-        </nav>
+        </div>
+      </div>
+      <div className="tw-navbar-center">
+        <a className="tw-btn tw-btn-ghost normal-case text-xl">{title}</a>
+      </div>
+      <div className="tw-navbar-end">
+        <button className="tw-btn tw-btn-ghost tw-btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        <button className="tw-btn tw-btn-ghost tw-btn-circle">
+          <div className="tw-indicator">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              />
+            </svg>
+            <span className="tw-badge tw-badge-xs tw-badge-primary tw-indicator-item"></span>
+          </div>
+        </button>
       </div>
     </header>
   );
