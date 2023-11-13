8f3cf05e137df43950a89f01cb75cbc7ce4e8939
Aman Mavai <amanmavai@gmail.com>
2023-11-13 19:10:32 +0530

    update card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,20 +2,21 @@ import * as React from "react";
 import {cn} from "../lib/utils";
 
 export type CardProps = {
+  title: React.ReactNode;
   imgUrl: string;
   cardBodyElement: React.ReactNode;
   cardActionElements: React.ReactNode[];
   className?: string;
 };
 
-export function Card({className, imgUrl, cardBodyElement, cardActionElements}: CardProps) {
+export function Card({title, imgUrl, cardBodyElement, cardActionElements, className}: CardProps) {
   return (
     <div className={cn("tw-card tw-card-compact bg-base-100 shadow-xl", className)}>
       <figure>
         <img src={imgUrl} alt="alt" />
       </figure>
       <div className="tw-card-body">
-        <h2 className="tw-card-title">Shoes!</h2>
+        <h2 className="tw-card-title">{title}</h2>
         {cardBodyElement}
         <div className="tw-card-actions justify-end">{cardActionElements}</div>
       </div>
